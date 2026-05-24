18975ea338dfb1e3a4f676e377d89d106b3baf24
pati702 <pati280399@gmail.com>
2026-05-24 13:27:54 +0200

    C: add a lot

--- asteroids/main.py
+++ asteroids/main.py
@@ -1,25 +1,38 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+    
     print("Starting Asteroids with pygame version: VERSION")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:
+        dt = clock.tick(60) / 1000      
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                return  
+        updatable.update(dt)
         screen.fill("black")
+        for da in drawable:
+            da.draw(screen)
+        #player.draw(screen)
         pygame.display.flip()
-        clock.tick(60)
-        dt = clock.tick() / 1000
+        print(dt)
+        
 
 
 if __name__ == "__main__":
